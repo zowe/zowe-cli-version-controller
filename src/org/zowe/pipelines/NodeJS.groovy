77f3c3ddfb6505b3d80530f0cdd17f87a4439b8a
Wright, Christopher R <christopher.wright@broadcom.com>
2019-01-11 14:00:20 -0500

    This could work

--- src/org/zowe/pipelines/NodeJS.groovy
+++ src/org/zowe/pipelines/NodeJS.groovy
@@ -1,16 +1,18 @@
 package org.zowe.pipelines
 
 class NodeJS {
-    def node
-    NodeJS(node) { this.node = node }
+    def steps
+    NodeJS(steps) { this.steps = steps }
 
     def setup(String test) {
         // steps.step('setup') {
         //     sh: "echo ${test}"
         // }
-        node.stages {
-            stage('setup') {
-                sh: "echo ${test}"
+        steps.node {
+            stages {
+                stage('setup') {
+                    sh: "echo ${test}"
+                }
             }
         }
     }
