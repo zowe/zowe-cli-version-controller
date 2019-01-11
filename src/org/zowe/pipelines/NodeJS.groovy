02be3feb43a06ce93a651e545ae5708946f1c07f
Wright, Christopher R <christopher.wright@broadcom.com>
2019-01-11 16:17:08 -0500

    Add more steps

--- src/org/zowe/pipelines/NodeJS.groovy
+++ src/org/zowe/pipelines/NodeJS.groovy
@@ -3,20 +3,29 @@ package org.zowe.pipelines
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 class NodeJS {
+    /**
+     * Store if the setup method was called
+     */
+    private boolean _setupCalled = false
+
     def steps
     NodeJS(steps) { this.steps = steps }
 
-    def setup(String test) {
+    def setup() {
         _createStage('checkout', {
             steps.checkout steps.scm
         })
 
         _createStage('setup', {
-            steps.sh "echo ${test}"
-        }, [shouldSkip: { -> true }])
+            steps.sh "echo SETUP HAPPENING"
+        })
+
+        _setupCalled = true
+
+        // @TODO ADD STEP TO SEND EMAIL OUT HERE
     }
 
-    // Define this function later
+    // document later
     private void _createStage(
         String name,
         Closure stepContents,
@@ -26,16 +35,18 @@ class NodeJS {
         def map = defaultMap << inputMap
         
         steps.stage(name) {
-            if (map.shouldSkip()) {
-                Utils.markStageSkippedForConditional(name);
-            } else {
-                steps.echo "Executing stage ${name}"
+            steps.timeout(time: map.timeout, unit: map.timeoutUnit) {
+                if (map.shouldSkip()) {
+                    Utils.markStageSkippedForConditional(name);
+                } else {
+                    steps.echo "Executing stage ${name}"
 
-                if (map.isSkipable) { // @TODO FILL STRING OUT
-                    steps.echo "Inform how to skip the step here"
+                    if (map.isSkipable) { // @TODO FILL STRING OUT
+                        steps.echo "Inform how to skip the step here"
+                    }
+
+                    stepContents()
                 }
-
-                stepContents()
             }
         }
     }
