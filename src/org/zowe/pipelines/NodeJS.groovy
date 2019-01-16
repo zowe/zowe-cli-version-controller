ac97c29dd1c60d5fda3e9266f085caf702353513
Wright, Christopher R <christopher.wright@broadcom.com>
2019-01-16 13:53:28 -0500

    Restructure and add end step

--- src/org/zowe/pipelines/NodeJS.groovy
+++ src/org/zowe/pipelines/NodeJS.groovy
@@ -32,11 +32,16 @@ public class NodeJS {
     private boolean _shouldSkipRemainingSteps = false
     private boolean _didBuild = false
 
+    def buildOptions = []
+
     // Map of all stages run
     private Map<String, Stage> _stages = [:]
 
     // Keeps track of the current stage
     private Stage _currentStage
+
+    // The first stage to execute
+    private Stage _firstStage
 
     // Keeps track of the first failing stage
     private Stage _firstFailingStage
@@ -58,16 +63,17 @@ public class NodeJS {
             createStage(name: 'Setup', stage: {
                 steps.echo "Setting up build configuration"
 
-                def opts = [];
                 def history = defaultBuildHistory;
 
+                // Add protected branch to build options
                 if (protectedBranches.containsKey(steps.BRANCH_NAME)) {
                     _isProtectedBranch = true;
                     history = protectedBranchBuildHistory
-                    opts.push(steps.disableConcurrentBuilds())
+                    buildOptions.push(steps.disableConcurrentBuilds())
                 }
 
-                opts.push(steps.buildDiscarder(steps.logRotator(numToKeepStr: history)))
+                // Add log rotator to build options
+                buildOptions.push(steps.buildDiscarder(steps.logRotator(numToKeepStr: history)))
                 steps.properties(opts)
             }, isSkipable: false)
 
@@ -122,50 +128,52 @@ public class NodeJS {
         _currentStage = stageInfo
 
         if (args.isSkipable) {
-            // Now get the control property option to see if the stage was forced skipped
-            steps.properties([
+            // Add the option to the build, this will be called in setup
+            buildOptions.push([
                 steps.parameters(
                     [
-                        steps.booleanParam(defaultValue: false, description: "Skip Stage: ${args.name}", name: args.name)
+                        steps.booleanParam(defaultValue: false, description: "Setting this to true will skip the stage \"${args.name}\"", name: "Skip Stage: ${args.name}")
                     ]
                 )
             ])
         }
 
-        steps.stage(args.name) {
-            try {
-                steps.timeout(time: args.timeoutVal, unit: args.timeoutUnit) {
-                    if (!_setupCalled) {
-                        steps.error("Pipeline setup not complete, please execute setup() on the instantiated NodeJS class")
-                    } else if (stageInfo.isSkippedByParam || _shouldSkipRemainingSteps || args.shouldSkip()) {
-                        Utils.markStageSkippedForConditional(args.name);
-                    } else {
-                        steps.echo "Executing stage ${args.name}"
+        stageInfo.execute = { 
+            steps.stage(args.name) {
+                try {
+                    steps.timeout(time: args.timeoutVal, unit: args.timeoutUnit) {
+                        if (!_setupCalled) {
+                            steps.error("Pipeline setup not complete, please execute setup() on the instantiated NodeJS class")
+                        } else if (stageInfo.isSkippedByParam || _shouldSkipRemainingSteps || args.shouldSkip()) {
+                            Utils.markStageSkippedForConditional(args.name);
+                        } else {
+                            steps.echo "Executing stage ${args.name}"
 
-                        stageInfo.wasExecuted = true
-                        
-                        if (args.isSkipable) { // @TODO FILL STRING OUT
-                            steps.echo "Inform how to skip the step here"
-                        }
+                            stageInfo.wasExecuted = true
+                            
+                            if (args.isSkipable) { // @TODO FILL STRING OUT
+                                steps.echo "Inform how to skip the step here"
+                            }
 
-                        def environment = []
+                            def environment = []
 
-                        // Add items to the environment if needed
-                        if (args.environment) {
-                            args.environment.each { key, value -> environment.push("${key}=${value}") }
-                        }
+                            // Add items to the environment if needed
+                            if (args.environment) {
+                                args.environment.each { key, value -> environment.push("${key}=${value}") }
+                            }
 
-                        // Run the passed stage with the proper environment variables
-                        steps.withEnv(environment) {
-                            args.stage()
+                            // Run the passed stage with the proper environment variables
+                            steps.withEnv(environment) {
+                                args.stage()
+                            }
                         }
                     }
+                } catch(e) {
+                    _firstFailingStage = stageInfo
+                    throw e
+                } finally {
+                    stageInfo.endOfStepBuildStatus = steps.currentBuild.currentResult
                 }
-            } catch(e) {
-                _firstFailingStage = stageInfo
-                throw e
-            } finally {
-                stageInfo.endOfStepBuildStatus = steps.currentBuild.currentResult
             }
         }
     }
@@ -212,6 +220,14 @@ public class NodeJS {
         createStage("test", {
             steps.echo "FILL THIS OUT"
         })
+    }
+
+    public void end() {
+        Stage stage = _firstStage
+
+        do {
+            stage.execute()
+        } while ((stage = stage.next) != null)
     }
 
     /**
@@ -261,4 +277,5 @@ class Stage {
     boolean wasExecuted = false
     String endOfStepBuildStatus // The result of the build at the end
     Stage next // The next stage
+    Closure execute // The closure to execute for the stage
 }
