c5ba53707d5ff31747d9e0b6ea544e910a6ec2e3
boech02 <christopher.boehm@ca.com>
2019-01-16 10:07:44 -0500

    test failure notification
    Signed-off-by: boech02 <christopher.boehm@ca.com>

--- src/org/zowe/pipelines/NodeJS.groovy
+++ src/org/zowe/pipelines/NodeJS.groovy
@@ -132,7 +132,7 @@ public class NodeJS {
         }
         catch (e) {
             // If there was an exception thrown, the build failed. Save the exception we encountered
-            steps.currentBuild.result = BUILD_UNSTABLE
+            steps.currentBuild.result = BUILD_FAILURE
             encounteredException = e
         }
     }
