e9959564d183796cc9a740b3848a5ca490c10bb5
boech02 <christopher.boehm@broadcom.com>
2019-01-18 15:14:36 -0500

    add additional mocked parameters to mocked steps class
    Signed-off-by: boech02 <christopher.boehm@broadcom.com>

--- src/test/groovy/org/zowe/pipelines/NodeJsTest.groovy
+++ src/test/groovy/org/zowe/pipelines/NodeJsTest.groovy
@@ -1,24 +1,56 @@
 package org.zowe.pipelines
 
 import groovy.org.zowe.pipelines.NodeJS
+import hudson.model.Result
 import org.junit.Test
 
-import static groovy.test.GroovyAssert.shouldFail
-
+/**
+ * Unit tests for the NodeJS class
+ */
 class NodeJsTest {
+    /**
+     * Test instantiating a NodeJS object
+     */
     @Test
-    void indexOutOfBoundsAccess() {
-        def numbers = [1, 2, 3, 4]
-        System.out.println("hello?")
+    void instantiationTest() {
         def steps = new MockSteps()
         def myNode = new NodeJS(steps)
-        System.out.println(myNode.notificationImages)
-        shouldFail {
-            numbers.get(99)
-        }
+        myNode.setup();
+    }
+
+    /**
+     * Test creating a new stage in the build
+     */
+    @Test
+    void createStageTest() {
+        def steps = new MockSteps()
+        def myNode = new NodeJS(steps)
+        myNode.setup()
+
+        myNode.createStage(name: "lint", stage: {
+            steps.sh "npm run lint"
+        })
+    }
+
+    /**
+     * Test creating a new stage in the build without calling setup
+     * (should cause an error)
+     */
+    @Test
+    void createStageWithoutSetupTest() {
+        def steps = new MockSteps()
+        def myNode = new NodeJS(steps)
+        // no call to setup
+        myNode.createStage(name: "lint", stage: {
+            steps.sh "npm run lint"
+        })
+
     }
 }
 
+/**
+ * Mocked class for "steps" Jenkins context
+ */
 class MockSteps {
     public echo(String echoStr) {
         System.out.println(echoStr)
@@ -27,4 +59,57 @@ class MockSteps {
     public error(String errStr) {
         throw new Exception(errStr)
     }
+
+    public checkout() {
+
+    }
+
+    public scm() {
+
+    }
+
+    public sh() {
+
+    }
+
+    public emailext(LinkedHashMap emailParams) {
+
+    }
+
+    public stage(String name, Closure closure) {
+
+    }
+
+    public timeout() {
+
+    }
+
+    public booleanParam(LinkedHashMap param) {
+
+    }
+
+    public logRotator(LinkedHashMap params) {
+
+    }
+
+    public buildDiscarder(LinkedHashMap params) {
+
+    }
+
+    public parameters(ArrayList params) {
+
+    }
+
+    public properties(ArrayList props) {
+
+    }
+
+    public Map params = [build: "hello"]
+    public Map currentBuild = [currentResult: "NOT_STARTED", result: Result.SUCCESS]
+
+    public Map env = [BUILD_NUMBER: "1", JOB_NAME: "Zowe Cli Version Controller"]
+
+    public String BRANCH_NAME = "Mocked_Branch"
+
+    public String RUN_DISPLAY_URL = "https://www.google.com"
 }
