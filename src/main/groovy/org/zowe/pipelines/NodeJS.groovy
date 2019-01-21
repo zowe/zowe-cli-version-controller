f7b2cff3d2f0f883060311d7b3df63a9d6514709
boech02 <christopher.boehm@broadcom.com>
2019-01-21 09:13:45 -0500

    fix package name
    Signed-off-by: boech02 <christopher.boehm@broadcom.com>

--- src/main/groovy/org/zowe/pipelines/NodeJS.groovy
+++ src/main/groovy/org/zowe/pipelines/NodeJS.groovy
@@ -1,6 +1,7 @@
-package org.zowe.pipelines
+package groovy.org.zowe.pipelines
 
 import hudson.model.Result
+
 // import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
 // @TODO enforce some sort of ordering
@@ -72,22 +73,6 @@ public class NodeJS {
 
     def steps
 
-    /**
-     * The result string for a successful build
-     */
-    def BUILD_SUCCESS = 'SUCCESS'
-
-    /**
-     * The result string for an unstable build
-     */
-    def BUILD_UNSTABLE = 'UNSTABLE'
-
-    /**
-     * The result string for a failed build
-     */
-    def BUILD_FAILURE = 'FAILURE'
-
-
     NodeJS(steps) { this.steps = steps }
 
     public void setup() {
@@ -142,7 +127,7 @@ public class NodeJS {
             if (_firstStage == null) {
                 // This is a condition that indicates that our logic is most likely broken
                 throw new StageException("First stage was not set but stages already had values in the map", stage.name)
-            } else if (!_firstFailingStage){
+            } else if (!_firstFailingStage) {
                 // The first stage should be setup, othewise a stage exception will be
                 // thrown before we get into here. So in setup, we should create the exception
                 // to be thrown later.
@@ -506,7 +491,8 @@ public class NodeJS {
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////// DATA FORMATS ////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-class StageArgs { // @TODO Stage minimum build health (if build health is >= to this minimum, continue with the stage else skip)
+class StageArgs {
+    // @TODO Stage minimum build health (if build health is >= to this minimum, continue with the stage else skip)
     String name
     Closure stage
     boolean isSkipable = true
