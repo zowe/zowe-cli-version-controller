9a29f81f348403b1f44bd96c4116d81bfc7a78aa
Wright, Christopher R <christopher.wright@broadcom.com>
2019-02-05 11:56:52 -0500

    Add documentation for changeinformation
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

--- src/org/zowe/pipelines/generic/models/ChangeInformation.groovy
+++ src/org/zowe/pipelines/generic/models/ChangeInformation.groovy
@@ -1,11 +1,37 @@
 package org.zowe.pipelines.generic.models
 
+/**
+ * State of the change information for a given git commit
+ */
 class ChangeInformation {
+    /**
+     * Is this change part of a pull request.
+     */
     final boolean isPullRequest
+
+    /**
+     * The branch name reported by the build.
+     */
     final String branchName
+
+    /**
+     * The base branch for a pull request.
+     *
+     * <p>This property will be null if {@link #isPullRequest} is false.</p>
+     */
     final String baseBranch
+
+    /**
+     * The change branch for a pull request.
+     *
+     * <p>This property will be null if {@link #isPullRequest} is false.</p>
+     */
     final String changeBranch
 
+    /**
+     * Construct the class.
+     * @param steps This is the workflow context that is used to determine the change status.
+     */
     ChangeInformation(def steps) {
         branchName = steps.BRANCH_NAME
 
