b0d31d866b51c9b3467f4b3ee571775c5b9f8591
Wright, Christopher R <christopher.wright@broadcom.com>
2019-02-07 11:03:09 -0500

    track the remote
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

--- src/org/zowe/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/pipelines/generic/GenericPipeline.groovy
@@ -264,7 +264,7 @@ class GenericPipeline extends Pipeline {
 
             // Setup the branch to track it's remote
             steps.sh "git branch ${_changeInfo.branchName}"
-            steps.sh "git checkout ${_changeInfo.branchName}"
+            steps.sh "git checkout ${_changeInfo.branchName} --track"
         }, isSkippable: false, timeout: [time: 1, unit: TimeUnit.MINUTES])
 
         createStage(name: 'Check for CI Skip', stage: {
