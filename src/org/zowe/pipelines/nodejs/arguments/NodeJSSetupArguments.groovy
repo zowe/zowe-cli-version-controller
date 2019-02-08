ce15d6e1c89a32e36bc271db935d5581856e2197
Wright, Christopher R <christopher.wright@broadcom.com>
2019-02-08 14:45:45 -0500

    also missed this
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

--- src/org/zowe/pipelines/nodejs/arguments/NodeJSSetupArguments.groovy
+++ src/org/zowe/pipelines/nodejs/arguments/NodeJSSetupArguments.groovy
@@ -1,7 +1,6 @@
 package org.zowe.pipelines.nodejs.arguments
 
 import org.zowe.pipelines.base.models.StageTimeout
-import org.zowe.pipelines.generic.args.GenericSetupArgs
 import org.zowe.pipelines.generic.arguments.GenericSetupArguments
 
 import java.util.concurrent.TimeUnit
