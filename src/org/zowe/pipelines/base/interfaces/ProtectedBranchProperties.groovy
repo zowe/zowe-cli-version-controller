e722bf560ecd3753cfe0163a86167a328a658a1d
Wright, Christopher R <christopher.wright@broadcom.com>
2019-02-08 09:58:55 -0500

    add documentation for class
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

--- src/org/zowe/pipelines/base/interfaces/ProtectedBranchProperties.groovy
+++ src/org/zowe/pipelines/base/interfaces/ProtectedBranchProperties.groovy
@@ -1,6 +1,13 @@
 package org.zowe.pipelines.base.interfaces
 
-// @TODO DOCUMENT
+/**
+ * Required properties of a model used in the {@link org.zowe.pipelines.base.ProtectedBranches}
+ * class.
+ */
 interface ProtectedBranchProperties {
+    /**
+     * The branch must have a name associated with it.
+     * @return The name of the branch
+     */
     String getName()
 }
