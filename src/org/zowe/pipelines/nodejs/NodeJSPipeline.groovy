3a6c882623ad04af28dae93680e16ed6ee8c0525
Wright, Christopher R <christopher.wright@broadcom.com>
2019-02-11 10:39:34 -0500

    test
    Signed-off-by: Wright, Christopher R <christopher.wright@broadcom.com>

--- src/org/zowe/pipelines/nodejs/NodeJSPipeline.groovy
+++ src/org/zowe/pipelines/nodejs/NodeJSPipeline.groovy
@@ -152,6 +152,7 @@ class NodeJSPipeline extends GenericPipeline {
 
             steps.dir("temp") {
                 steps.sh "ARCHIVE_NAME=\$(npm pack ../ | tail -1)"
+                steps.sh "printenv"
                 steps.archiveArtifacts steps.env.ARCHIVE_NAME
                 steps.sh "rm -f \$ARCHIVE_NAME"
             }
