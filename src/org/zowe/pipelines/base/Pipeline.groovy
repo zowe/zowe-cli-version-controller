9d99020fd04daa5d1d3fb31b7772c4dbece387c2
Swauger, William E <William.Swauger@ca.com>
2019-04-30 15:21:01 -0400

    Retrieve username password attempt 5

--- src/org/zowe/pipelines/base/Pipeline.groovy
+++ src/org/zowe/pipelines/base/Pipeline.groovy
@@ -569,12 +569,14 @@ class Pipeline {
         }
 
         createStage(name: 'Validate labels', stage: {
+
+
             steps.withCredentials([steps.usernamePassword(
               credentialsId: gitConfig.credentialsId,
               passwordVariable: 'PASSWORD',
               usernameVariable: 'USERNAME'
             )]) {
-                _verifyReleaseLabel("name", $USERNAME, $PASSWORD,"https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels")
+                _verifyReleaseLabel("name", USERNAME, PASSWORD,"https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels")
             }
 
             // Setup the branch to track it's remote
