8f6e947939be909c0e591eee760e153b7ca6da4c
Swauger, William E <William.Swauger@ca.com>
2019-04-30 10:11:33 -0400

    Removed unnecessary 'sh'

--- src/org/zowe/pipelines/base/Pipeline.groovy
+++ src/org/zowe/pipelines/base/Pipeline.groovy
@@ -575,8 +575,8 @@ class Pipeline {
 //            )])
 
             // Setup the branch to track it's remote
-            steps.sh 'sh groovy verifyReleaseLabel.groovy "name" "ws617385" "Peter234*" "https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels"'
-            //steps.sh "sh verifyReleaseLabel.sh 'name' 'ws617385' 'Peter234*' 'https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels'"
+            steps.sh 'groovy verifyReleaseLabel.groovy "name" "ws617385" "Peter234*" "https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels"'
+            //steps.sh "verifyReleaseLabel.sh 'name' 'ws617385' 'Peter234*' 'https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels'"
 
         }, isSkippable: false, timeout: timeouts.checkout,)
 
