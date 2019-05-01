f0d5e7d6336790ef0929f8c08e9e563b93aa38c2
zFernand0 <Fernando.RijoCedeno@broadcom.com>
2019-05-01 14:56:36 -0400

    remove debug labels for now
    Signed-off-by: zFernand0 <Fernando.RijoCedeno@broadcom.com>

--- src/org/zowe/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/pipelines/generic/GenericPipeline.groovy
@@ -372,7 +372,6 @@ class GenericPipeline extends Pipeline {
                         passwordVariable: "PASSWORD",
                         usernameVariable: "USERNAME"
                 )]) {
-                    steps.sh "echo \"${gitConfig.credentialsId.substring(1)}\""
                     _verifyReleaseLabel("name", "\$USERNAME", "\$PASSWORD","https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/issues/2/labels")
                 }
             }
@@ -885,7 +884,6 @@ class GenericPipeline extends Pipeline {
 
         // retrieve label names from pull request
         def userpass = "$user:$password"
-        steps.sh "echo \"${userpass.substring(1)}\""
         def process = steps.sh script: "curl -u\"${userpass}\" -X GET -H \"Content-Type: application/json\" $url", returnStdout: true
 
         // pull the label names out
