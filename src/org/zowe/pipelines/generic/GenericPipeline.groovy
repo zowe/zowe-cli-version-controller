0ed7e7dabbc270ebcd3a07ec99d8844932ec8ede
zFernand0 <Fernando.RijoCedeno@broadcom.com>
2019-05-01 14:36:36 -0400

    get labels for the right pr
    Signed-off-by: zFernand0 <Fernando.RijoCedeno@broadcom.com>

--- src/org/zowe/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/pipelines/generic/GenericPipeline.groovy
@@ -372,7 +372,7 @@ class GenericPipeline extends Pipeline {
                         passwordVariable: "PASSWORD",
                         usernameVariable: "USERNAME"
                 )]) {
-                    _verifyReleaseLabel("name", "\$USERNAME", "\$PASSWORD","https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels")
+                    _verifyReleaseLabel("name", "\$USERNAME", "\$PASSWORD","https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/issues/2/labels")
                 }
             }
             args.operation(stageName)
