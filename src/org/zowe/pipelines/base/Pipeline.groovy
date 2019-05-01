04e4a63b048d66cfd2d921e072f54555ca02ccd5
Swauger, William E <William.Swauger@ca.com>
2019-05-01 09:44:35 -0400

    Retrieve username password attempt, attempt to display Environmental variables

--- src/org/zowe/pipelines/base/Pipeline.groovy
+++ src/org/zowe/pipelines/base/Pipeline.groovy
@@ -577,7 +577,7 @@ class Pipeline {
               usernameVariable: "USERNAME"
             )]) {
                 steps.echo gitConfig.credentialsId
-                steps.echo "USERNAME"
+                steps.echo USERNAME
                 steps.echo "PASSWORD"
                // _verifyReleaseLabel("name", USERNAME, PASSWORD,"https://github.gwd.broadcom.net/api/v3/repos/ws617385/playground/labels")
             }
