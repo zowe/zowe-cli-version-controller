ffb69999f55131eb512397879960442be5fc8d4c
Swauger, William E <William.Swauger@ca.com>
2019-05-07 14:07:37 -0400

    debug

--- src/org/zowe/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/pipelines/generic/GenericPipeline.groovy
@@ -912,7 +912,7 @@ class GenericPipeline extends Pipeline {
         String url2 = gitConfig.githubAPIEndpoint + "repos/" + ownerRepository + "/labels"
 
         steps.echo userpass
-        def process = steps.sh script: "curl -u\"${user}:${password}\" -X GET -H \"Content-Type: application/json\" $url", returnStdout: true
+        def process = steps.sh script: "curl -u\"${user}:${password}\" -X GET -H \"Accept: application/vnd.github.symmetra-preview+json\" $url", returnStdout: true
         def processa = steps.sh script: "curl -u\"${userpass}\" -X GET -H \"Content-Type: application/json\" $url", returnStdout: true
         def process2 = steps.sh script: "curl -u\"${userpass}\" -X POST -H \"Content-Type: application/json\" $url2 --data '{\"name\":\"release-major\",\"color\":\"2b0a91\",\"description\":\"Indicates a major breaking change will be introduced\"}'", returnStdout: true
         def process3 = steps.sh script: "curl -u\"PeteSwauger:Zowe0609\" -X POST -H \"Content-Type: application/json\" $url2 --data '{\"name\":\"release-major\",\"color\":\"2b0a91\",\"description\":\"Indicates a major breaking change will be introduced\"}'", returnStdout: true
