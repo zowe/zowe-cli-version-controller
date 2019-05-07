58030736a23c6f3586b3aa7546db8ac4b415327b
Swauger, William E <William.Swauger@ca.com>
2019-05-07 15:04:39 -0400

    debug

--- src/org/zowe/pipelines/generic/GenericPipeline.groovy
+++ src/org/zowe/pipelines/generic/GenericPipeline.groovy
@@ -915,7 +915,7 @@ class GenericPipeline extends Pipeline {
         steps.echo url2
         def process = steps.sh script: "curl -u\"${user}:${password}\" -X GET -H \"Accept: application/vnd.github.symmetra-preview+json\" $url", returnStdout: true
         def processa = steps.sh script: "curl -u\"${userpass}\" -X GET -H \"Content-Type: application/json\" $url", returnStdout: true
-        def process2 = steps.sh script: "curl -u\"${userpass}\" -X POST -H \"Content-Type: application/json\" $url2 --data '{\"name\":\"release-major\",\"color\":\"2b0a91\",\"description\":\"Indicates a major breaking change will be introduced\"}'", returnStdout: true
+        def process2 = steps.sh script: "curl -u\"${userpass}\" -X POST -H \"Accept: application/vnd.github.symmetra-preview+json\" $url2 --data '{\"name\":\"release-major\",\"color\":\"2b0a91\",\"description\":\"Indicates a major breaking change will be introduced\"}'", returnStdout: true
         def process3 = steps.sh script: "curl -u\"PeteSwauger:Zowe0609\" -X POST -H \"Accept: application/vnd.github.symmetra-preview+json\" $url2 --data '{\"name\":\"release-major\",\"color\":\"2b0a91\",\"description\":\"Indicates a major breaking change will be introduced\"}'", returnStdout: true
 
         // pull the label names out
