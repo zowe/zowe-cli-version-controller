96bedf3dba288f7f2d8b0c82278b231cf1e3d9ff
zFernand0 <Fernando.RijoCedeno@broadcom.com>
2019-06-13 11:10:22 -0400

    ultimate installation

--- scripts/repackage_tar.sh
+++ scripts/repackage_tar.sh
@@ -38,7 +38,7 @@ if [ -e "npm-shrinkwrap.json" ]; then
     # debug
     cat npm-shrinkwrap.json | grep perf-timing
 
-    npm install --registry "$registry" --only=prod
+    npm install --only=prod
 
     gizafile="gizapackages.txt"
     awk '/giza/ { print $2 }' npm-shrinkwrap.json | awk -F"@" '{ print $3 }' | awk -F".tgz" '{ print $1 }'| awk NF > $gizafile
@@ -48,10 +48,18 @@ if [ -e "npm-shrinkwrap.json" ]; then
     node -e "package = require('./package.json');var logger = require('fs').createWriteStream('$depsfile', {flags:'a'});for(pkg in package.dependencies){if(pkg.indexOf('@') >= 0)logger.write(pkg + ' ' + package.dependencies[pkg] + '\n');};logger.end();"
     cat $depsfile
 
-    install_package(){
-        echo $"hi $1 --- $2 --- $3\n"
-    }
-    xargs install_package < $depsfile
+    while read p1; do
+        echo "$p1"
+        tpkg=$(echo $p1 | cut -d' ' -f 1)
+        tver=$(echo $p1 | cut -d' ' -f 2)
+        temppkg="@$tpkg@$tver"
+        echo "$tpkg --- $tver --- $temppkg"
+        while read p2; do
+            if [[ $p2 == *$tpkg* ]]; then
+                npm install $temppkg --registry $registry --force
+            fi
+        done < $gizafile
+    done < $depsfile
 
     # debug
     cat npm-shrinkwrap.json | grep perf-timing
