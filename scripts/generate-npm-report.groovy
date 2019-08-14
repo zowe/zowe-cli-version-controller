3d570ba95638ca56541c0f256e797dfca2d4452f
zFernand0 <fernando.rijocedeno@broadcom.com>
2019-08-14 11:18:39 -0400

    add tag detection and other stuff
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- scripts/generate-npm-report.groovy
+++ scripts/generate-npm-report.groovy
@@ -20,7 +20,7 @@ def opts = []
 opts.push(
   parameters([
     string(name: 'ORG', defaultValue: 'zowe', description: 'Organization to gather packages from'),
-    string(name: 'EXTRA_REPOS', defaultValue: 'vscode-extension-for-zowe', description: 'Add additional repositories under Zowe on Public GitHub, (comma-separated)'),
+    string(name: 'EXTRA_REPOS', defaultValue: 'zowe/vscode-extension-for-zowe:master', description: 'Add additional repositories on Public GitHub<br> It needs to be comma-separated<br>Format: user/repo-name:branch'),
     string(name: 'RECIPIENTS_LIST', defaultValue: '', description: 'List of emails to recevie build resutls (Override)')
   ])
 )
@@ -34,50 +34,76 @@ properties(opts)
 def expectJSON(shellScript) {
   def _temp = sh returnStdout: true, script: shellScript
   def temp = readJSON text: _temp
-  if (!temp) throw "Unexpected JSON format: ${_temp}"
+  if (!temp) throw "Unexpected JSON format: $_temp"
   return temp
 }
 
+def getFileName(isRepo, name, tempEnv, pkgTag, format = 'json') {
+  if (isRepo) {
+    return "repo.$name.$tempEnv.$pkgTag.$format"
+  } else {
+    return "pkg.$name.$tempEnv.$pkgTag.$format"
+  } 
+}
+
+def _reportHelper(isRepo, name, pkgJson, pkgTag) {
+  def deps = pkgJson.dependencies
+  def devDeps = pkgJson.devDependencies
+  sh "rm -rf $name || exit 0"
+  sh "mkdir $name"
+  dir("$name") {
+    writeJSON json: pkgJson, file: "package.json"
+    sh "npm install --package-lock-only"
+    sh "npm audit --json > ../${getFileName(isRepo, name, 'all', pkgTag)} || exit 0"
+    sh "mkdir dev prod"
+
+    pkgJson.dependencies = [:]
+    pkgJson.devDependencies = devDeps
+    dir("dev") {
+      writeJSON json: pkgJson, file: "package.json"
+      sh "npm install --package-lock-only"
+      sh "npm audit --json > ../../${getFileName(isRepo, name, 'dev', pkgTag)} || exit 0"
+    }
+
+    pkgJson.dependencies = deps
+    pkgJson.devDependencies = [:]
+    dir("prod") {
+      writeJSON json: pkgJson, file: "package.json"
+      sh "npm install --package-lock-only"
+      sh "npm audit --json > ../../${getFileName(isRepo, name, 'prod', pkgTag)} || exit 0"
+    }
+  }
+}
+
+// TODO: Publish to advisory
+/*
+NpmAuditReports/zowe-cli/lts-incremental/pkg-name.all.json
+NpmAuditReports/zowe-cli/lts-incremental/pkg-name.prod.json
+NpmAuditReports/zowe-cli/lts-incremental/pkg-name.dev.json
+NpmAuditReports/zowe-cli/latest/pkg-name.all.json
+NpmAuditReports/zowe-cli/latest/pkg-name.prod.json
+NpmAuditReports/zowe-cli/latest/pkg-name.dev.json
+*/
+
 def generateReport(name, isRepo = false) {
   return {
-    stage("Generate report: ${name}") {
-      def (pkgJson, tempDir, deps, devDeps) = ["", ""]
+    stage("Generate report: $name") {
       if (isRepo) {
-        tempDir = "${name}"
-        pkgJson = expectJSON("curl https://raw.githubusercontent.com/zowe/${name}/master/package.json")
+        def branchName = "${name.split(':')[1]}" // get branch name
+        name = "${name.split(':')[0]}" // remove branch name
+        def pkgJson = expectJSON("curl https://raw.githubusercontent.com/$name/$branchName/package.json")
+        name = name.split('/')[1] // remove user name
+        _reportHelper(isRepo, name, pkgJson, branchName)
       } else {
-        tempDir = "${name.split('/')[1]}"
-        pkgJson = expectJSON("npm view ${name} --json")
-      }
-      deps = pkgJson.dependencies
-      devDeps = pkgJson.devDependencies
-      sh "rm -rf ${tempDir} || exit 0"
-      sh "mkdir ${tempDir}"
-      dir("${tempDir}") {
-        sh "npm install --package-lock-only"
-        sh "npm audit --json > ${tempDir}.all.json || exit 0"
-        sh "mkdir dev prod"
-
-        pkgJson.dependencies = [:]
-        pkgJson.devDependencies = devDeps
-        dir("dev"){
-          writeJSON json: pkgJson, file: "package.json"
-          sh "cat package.json"
-          sh "npm install --package-lock-only"
-          sh "npm audit --json > ../${tempDir}.dev.json || exit 0"
+        def tags = expectJSON("npm view $name dist-tags --json")
+        tag.each { tag, ver ->
+          def pkgJson = expectJSON("npm view $name@$tag --json")
+          name = name.split('/')[1] // remove org name
+          _reportHelper(isRepo, name, pkgJson, tag)
         }
-
-        pkgJson.dependencies = deps
-        pkgJson.devDependencies = [:]
-        dir("prod"){
-          writeJSON json: pkgJson, file: "package.json"
-          sh "cat package.json"
-          sh "npm install --package-lock-only"
-          sh "npm audit --json > ../${tempDir}.prod.json || exit 0"
-        }
-        sh "ls -al"
-        sh "cat *.json"
       }
+      sh "tar -czvf reports.tgz *.json"
+      archiveArtifacts artifacts: "reports.tgz"
     }
   }
 }
@@ -93,6 +119,7 @@ node('ca-jenkins-agent') {
       buildStages.put(repoName, generateReport(repoName, true))
     }
     sh "npm config set @brightside:registry https://api.bintray.com/npm/ca/brightside"
+
     parallel(buildStages)
   } catch (e) {
     currentBuild.result = "FAILURE"
@@ -103,7 +130,7 @@ node('ca-jenkins-agent') {
       to: recipients,
       subject: "[${currentBuild.currentResult}] Reports generated. ORG: ${params.ORG}",
       body: """
-      <p>Build result: ${currentBuild.absoluteUrl}</p>
+      Build result: ${currentBuild.absoluteUrl}
       """
     );
   }
