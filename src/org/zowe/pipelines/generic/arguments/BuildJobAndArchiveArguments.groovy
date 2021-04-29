f8427a2158a4b526c540f86658fec0cb837fde78
zFernand0 <fernando.rijocedeno@broadcom.com>
2021-04-29 11:26:58 -0400

    Build Job and Archive: initial setup
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

---
+++ src/org/zowe/pipelines/generic/arguments/BuildJobAndArchiveArguments.groovy
@@ -0,0 +1,34 @@
+/*
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
+package org.zowe.pipelines.nodejs.arguments
+
+import org.zowe.pipelines.generic.arguments.GenericStageArguments
+
+/**
+ * Represents the arguments available to the
+ * {@link org.zowe.pipelines.nodejs.NodeJSPipeline#version(java.util.Map)} method.
+ */
+class BuildJobAndArchiveArguments extends GenericStageArguments {
+    /**
+     * The name of the job to be triggered
+     */
+    String jobName = ""
+
+    /**
+     * The parameters to be sent to the job being triggered
+     */
+    Map<String, Object> jobParms = [:]
+
+    /**
+     * Specifies if the results of the remote job should be propagated
+     */
+    Boolean propagate = false
+}
